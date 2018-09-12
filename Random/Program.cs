bcb1d497fc7da6c63fdc3ef1e338e352b0618213
Roberto Deresu <ioan-roberto.deresu@my.fmi.unibuc.ro>
2018-09-12 21:18:14 +0300

    Implementation of random.

--- Random/Program.cs
+++ Random/Program.cs
@@ -1,12 +1,30 @@
 ﻿using System;
+using System.Text;
 
 namespace Random
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Random text will be set to clipboard. Enter the size of random text: ");
+            var randomSize = int.Parse(Console.ReadLine());
+            var random = CreateRandom(randomSize);
+            Clipboard.Copy(random);
+        }
+
+        private static string CreateRandom(int length)
+        {
+            const string valid = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*(){}";
+            var res = new StringBuilder();
+            var rnd = new System.Random();
+
+            while (0 < length--)
+            {
+                res.Append(valid[rnd.Next(valid.Length)]);
+            }
+
+            return res.ToString();
         }
     }
 }
